32e1e49c1808ff3a941cd99afba842d7e1943534
liuboliu <2534660131@qq.com>
2020-04-06 15:08:31 +0800

    zheli

---
+++ CollectionReusableView.h
@@ -0,0 +1,16 @@
+//
+//  CollectionReusableView.h
+//  LXBTestKit
+//
+//  Created by Apple on 2020/4/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CollectionReusableView : UICollectionReusableView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CollectionReusableView.m
@@ -0,0 +1,12 @@
+//
+//  CollectionReusableView.m
+//  LXBTestKit
+//
+//  Created by Apple on 2020/4/6.
+//
+
+#import "CollectionReusableView.h"
+
+@implementation CollectionReusableView
+
+@end
